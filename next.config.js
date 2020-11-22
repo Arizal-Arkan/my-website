a4b8e357e441ac464863167ad75815d2dddba3ef
Arizal Arkan <arizlarkan@gmail.com>
2020-11-22 17:40:57 +0700

    fix prefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
 // next.config.js
 const withImages = require("next-images");
-module.exports = {
-  assetPrefix: "/my-website/",
-};
-
-module.exports = withImages();
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
+module.exports = withPlugins([
+  [
+    optimizedImages,
+    {
+      mozjpeg: {
+        quality: 80,
+      },
+      pngquant: {
+        speed: 3,
+        strip: true,
+        verbose: true,
+      },
+      imagesPublicPath: "/my-website/_next/static/images/",
+    },
+  ],
+  withImages(),
+  {
+    basePath: "/my-website",
+    assetPrefix: "/my-website/",
+  },
+]);
