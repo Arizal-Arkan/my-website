eca6107ddfb99174d4e7391eb2146bc7df521a4c
Arizal Arkan <arizlarkan@gmail.com>
2020-11-22 17:26:40 +0700

    fix prefix

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 // next.config.js
-const withImages = require('next-images')
-module.exports = withImages()
+const withImages = require("next-images");
+module.exports = {
+  assetPrefix: "/wallisconsultancy/",
+};
+
+module.exports = withImages();
